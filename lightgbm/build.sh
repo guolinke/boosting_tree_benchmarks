12f18efb23f961ae74ff34ef459ccfb5214745a1
Guolin Ke <guoke@microsoft.com>
2016-10-26 16:30:26 +0800

    add scripts for run and build

---
+++ lightgbm/build.sh
@@ -0,0 +1,4 @@
+git clone --recursive https://github.com/Microsoft/LightGBM ; cd LightGBM
+mkdir build ; cd build
+cmake .. 
+make -j 
---
+++ xgboost/build.sh
@@ -0,0 +1,2 @@
+git clone --recursive https://github.com/dmlc/xgboost
+cd xgboost; make -j4
