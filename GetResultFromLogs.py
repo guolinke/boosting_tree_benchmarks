11090c969c84b3ad9dbdf9f226fa8f422c00ccbb
Guolin Ke <i@yumumu.me>
2016-12-03 15:41:39 +0800

    update lightgbm result

--- GetResultFromLogs.py
+++ GetResultFromLogs.py
@@ -9,8 +9,8 @@ def GetAccuracyFromLightGBM(filename, key):
 	input = open(filename, "r")
 	ret = 0.0
 	for line in input.readlines():
-		if key in line:
-			ret = max(ret,float(line.split(key+':')[1].split('\t')[0]))
+		if key + ' :' in line:
+			ret = max(ret,float(line.split(key+' :')[-1]))
 	return ret
 def GetTimeFromXgboost(filename):
 	input = open(filename, "r")
@@ -33,15 +33,15 @@ lightgbm_speed_result = [GetTimeFromLightGBM('lightgbm/lightgbm_higgs_speed.log'
 
 lightgbm_higgs_accuracy = [GetAccuracyFromLightGBM('lightgbm/lightgbm_higgs_accuracy.log', "auc")]
 
-lightgbm_yahoo_accuracy = [GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "NDCG@1")
-,GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "NDCG@3")
-,GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "NDCG@5")
-,GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "NDCG@10")]
+lightgbm_yahoo_accuracy = [GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "ndcg@1")
+,GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "ndcg@3")
+,GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "ndcg@5")
+,GetAccuracyFromLightGBM('lightgbm/lightgbm_yahoo_accuracy.log', "ndcg@10")]
 
-lightgbm_msltr_accuracy = [GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "NDCG@1")
-,GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "NDCG@3")
-,GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "NDCG@5")
-,GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "NDCG@10")]
+lightgbm_msltr_accuracy = [GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "ndcg@1")
+,GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "ndcg@3")
+,GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "ndcg@5")
+,GetAccuracyFromLightGBM('lightgbm/lightgbm_msltr_accuracy.log', "ndcg@10")]
 
 xgboost_speed_result = [GetTimeFromXgboost('xgboost/xgboost_higgs_speed.log')
 	,GetTimeFromXgboost('xgboost/xgboost_yahoo_speed.log')
@@ -81,9 +81,9 @@ Title = ["Higgs", "Yahoo LTR", "MS LTR"]
 speed_result = [xgboost_speed_result, xgboost_approx_speed_result, lightgbm_speed_result]
 output.write('| Data      |  xgboost| xgboost_approx |  LightGBM|\n')
 output.write('|----|  ----| ---- |  ----|\n')
-for i in xrange(len(Title)):
+for i in range(len(Title)):
 	output.write('| ' + Title[i] + '|')
-	for j in xrange(len(speed_result)):
+	for j in range(len(speed_result)):
 		output.write(str(speed_result[j][i]) + 's |')
 	output.write('\n')
 
@@ -94,36 +94,36 @@ Title = ["AUC"]
 speed_result = [xgboost_higgs_accuracy, xgboost_approx_higgs_accuracy, lightgbm_higgs_accuracy]
 output.write('| Metric      |  xgboost| xgboost_approx |  LightGBM|\n')
 output.write('| ----------- |  -------| -------------- |  --------|\n')
-for i in xrange(len(Title)):
+for i in range(len(Title)):
 	output.write('| ' + Title[i] + '|')
-	for j in xrange(len(speed_result)):
+	for j in range(len(speed_result)):
 		output.write(str(speed_result[j][i]) + '|')
 	output.write('\n')
 
 output.write('\n\n')
 
-output.write("NDCG at Yahoo LTR:\n\n")
-Title = ["NDCG@1","NDCG@3","NDCG@5","NDCG@10"]
+output.write("ndcg at Yahoo LTR:\n\n")
+Title = ["ndcg@1","ndcg@3","ndcg@5","ndcg@10"]
 speed_result = [xgboost_yahoo_accuracy, xgboost_approx_yahoo_accuracy, lightgbm_yahoo_accuracy]
 output.write('| Metric      |  xgboost| xgboost_approx |  LightGBM|\n')
 output.write('| ----------- |  -------| -------------- |  --------|\n')
-for i in xrange(len(Title)):
+for i in range(len(Title)):
 	output.write('| ' + Title[i] + '|')
-	for j in xrange(len(speed_result)):
+	for j in range(len(speed_result)):
 		output.write(str(speed_result[j][i]) + '|')
 	output.write('\n')
 
 output.write('\n\n')
 
 
-output.write("NDCG at MS LTR:\n\n")
-Title = ["NDCG@1","NDCG@3","NDCG@5","NDCG@10"]
+output.write("ndcg at MS LTR:\n\n")
+Title = ["ndcg@1","ndcg@3","ndcg@5","ndcg@10"]
 speed_result = [xgboost_msltr_accuracy, xgboost_approx_msltr_accuracy, lightgbm_msltr_accuracy]
 output.write('| Metric      |  xgboost| xgboost_approx |  LightGBM|\n')
 output.write('| ----------- |  -------| -------------- |  --------|\n')
-for i in xrange(len(Title)):
+for i in range(len(Title)):
 	output.write('| ' + Title[i] + '|')
-	for j in xrange(len(speed_result)):
+	for j in range(len(speed_result)):
 		output.write(str(speed_result[j][i]) + '|')
 	output.write('\n')
 
