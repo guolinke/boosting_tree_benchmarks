e07f93fd4eccd5320417e3afba567d885f3b49f2
Guolin Ke <i@yumumu.me>
2017-01-21 10:04:04 +0800

    update latest result

--- xgboost/test_speed.sh
+++ xgboost/test_speed.sh
@@ -9,9 +9,10 @@ xgboost/xgboost xgboost.conf tree_method=approx sketch_eps=0.004 data="../data/m
 xgboost/xgboost xgboost.conf tree_method=approx sketch_eps=0.004 data="../data/yahoo.train" objective="rank:ndcg" 2>&1 | tee xgboost_approx_yahoo_speed.log
 xgboost/xgboost xgboost.conf tree_method=approx sketch_eps=0.004 data="../data/dataexpo_onehot.train" objective="binary:logistic" 2>&1 | tee xgboost_approx_dataexpo_onehot_speed.log
 
-xgboost/xgboost xgboost.conf tree_method=hist grow_policy=lossguide eta=0.1 gamma=1.0 max_depth=0 max_leaves=255 min_child_weight=100 data="../data/higgs.train" objective="binary:logistic" 2>&1 | tee xgboost_hist_higgs_speed.log
-xgboost/xgboost xgboost.conf tree_method=hist grow_policy=lossguide eta=0.1 gamma=1.0 max_depth=0 max_leaves=255 min_child_weight=100  data="../data/msltr.train" objective="rank:ndcg" 2>&1 | tee xgboost_hist_msltr_speed.log
-xgboost/xgboost xgboost.conf tree_method=hist grow_policy=lossguide eta=0.1 gamma=1.0 max_depth=0 max_leaves=255 min_child_weight=100  data="../data/yahoo.train" objective="rank:ndcg" 2>&1 | tee xgboost_hist_yahoo_speed.log
+xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/higgs.train" objective="binary:logistic" 2>&1 | tee xgboost_hist_higgs_speed.log
+xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/msltr.train" objective="rank:ndcg" 2>&1 | tee xgboost_hist_msltr_speed.log
+xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/yahoo.train" objective="rank:ndcg" 2>&1 | tee xgboost_hist_yahoo_speed.log
+xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/dataexpo_onehot.train" objective="binary:logistic"  2>&1 | tee xgboost_hist_dataexpo_onehot_speed.log
 
 
 
