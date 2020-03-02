8decc871530a9ccd6800b234a4e4ac6bda4291ac
guolinke <guolin.ke@outlook.com>
2020-03-02 22:22:49 +0800

    update benchmark results

--- lightgbm/test_speed.sh
+++ lightgbm/test_speed.sh
@@ -1,8 +1,8 @@
 LightGBM/lightgbm config=lightgbm.conf data=../data/higgs.train objective=binary 2>&1 | tee lightgbm_higgs_speed.log
 
-LightGBM/lightgbm config=lightgbm.conf data=../data/msltr.train objective=lambdarank 2>&1 | tee lightgbm_msltr_speed.log
+LightGBM/lightgbm config=lightgbm.conf data=../data/msltr.train objective=regression 2>&1 | tee lightgbm_msltr_speed.log
 
-LightGBM/lightgbm config=lightgbm.conf data=../data/yahoo.train objective=lambdarank 2>&1 | tee lightgbm_yahoo_speed.log
+LightGBM/lightgbm config=lightgbm.conf data=../data/yahoo.train objective=regression 2>&1 | tee lightgbm_yahoo_speed.log
 
 LightGBM/lightgbm config=lightgbm.conf data=../data/dataexpo_onehot.train objective=binary  2>&1 | tee lightgbm_dataexpo_speed.log
 
--- xgboost/test_speed.sh
+++ xgboost/test_speed.sh
@@ -6,8 +6,8 @@ xgboost/xgboost xgboost.conf tree_method=exact data="../data/allstate.train" eta
 
 
 xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/higgs.train" objective="binary:logistic" 2>&1 | tee xgboost_hist_higgs_speed.log
-xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/msltr.train" objective="rank:ndcg" 2>&1 | tee xgboost_hist_msltr_speed.log
-xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/yahoo.train" objective="rank:ndcg" 2>&1 | tee xgboost_hist_yahoo_speed.log
+xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/msltr.train" objective="reg:squarederror" 2>&1 | tee xgboost_hist_msltr_speed.log
+xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/yahoo.train" objective="reg:squarederror" 2>&1 | tee xgboost_hist_yahoo_speed.log
 xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=255 data="../data/dataexpo_onehot.train" objective="binary:logistic"  2>&1 | tee xgboost_hist_dataexpo_speed.log
 xgboost/xgboost xgboost.conf max_bin=255 tree_method=hist grow_policy=lossguide max_depth=0 max_leaves=127 eta=0.02 data="../data/allstate.train" objective="binary:logistic"  2>&1 | tee xgboost_hist_allstate_speed.log
 
